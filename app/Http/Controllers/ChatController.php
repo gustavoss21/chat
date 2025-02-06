a649d16bcb8daa19d90d90b3c59134db683b888a
gustavo souza <gustavosantossouza65@gmail.com>
2025-02-06 20:10:27 -0300

    refatorando as rotas

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -11,17 +11,17 @@ class ChatController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $messages = Message::where('sender_user_id',$request->get('sender_user_id'))
+        ->where('recipient_user_id',$request->get('recipient_user_id'))
+        ->orWhere(function($query) use($request){
+            $query->where('recipient_user_id',$request->get('sender_user_id'))
+            ->where('sender_user_id',$request->get('recipient_user_id'));
+        })
+        ->get();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return response()->json($messages);
     }
 
     /**
@@ -29,63 +29,8 @@ class ChatController extends Controller
      */
     public function store(Request $request)
     {
-        $index_message = Message::Create($request->all());
+        $message = Message::Create($request->all());
 
-        if(!$index_message->count() > 0){
-            return response('the message no was created',400);
-        }
-
-        return response('message created');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Message $message)
-    {
-        return response($message);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Message $message)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Message $message)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Message $message)
-    {
-        //
-    }
-
-    public function messages(Request $request){
-
-        $messages = Message::where('sender_user_id',$request->get('sender_user_id'))
-                ->where('recipient_user_id',$request->get('recipient_user_id'))
-                ->orWhere(function($query) use($request){
-                    $query->where('recipient_user_id',$request->get('sender_user_id'))
-                    ->where('sender_user_id',$request->get('recipient_user_id'));
-                })
-                ->get();
-        
-        return response()->json($messages);
-
-    }
-
-    public function sendMessage(Request $request){
-        $message = Message::create($request->all());
         return response()->json($message);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::controller(ChatController::class)->group(function () {
-    Route::get('/chat', 'messages');
-    Route::post('/chat', 'sendMessage');
-});
-
 Route::get('/users', [ProfileController::class,'contacts']);
 
-Route::apiResource('message',ChatController::class);
+Route::apiResource('chat',ChatController::class)
+    ->only([
+        'index','store'
+    ]);;
